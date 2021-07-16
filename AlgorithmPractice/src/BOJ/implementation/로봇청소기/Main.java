1f677b5b16ac5e9362ed3045048e583ba2e3dc5c
sangkipark <sangki931@gmail.com>
2021-07-16 22:30:12 +0900

    로봇 청소기 commit

--- AlgorithmPractice/src/BOJ/implementation/로봇청소기/Main.java
+++ AlgorithmPractice/src/BOJ/implementation/로봇청소기/Main.java
@@ -10,11 +10,11 @@ public class Main {
 
 	static int n;
 	static int m;
-	public static boolean isInTheMap(int r,int c) {
-		if(0<=r && 0<=c && r<n && r<m)
-			return true;
-		return false;
-	}
+	static final int NORTH = 0;
+	static final int SOUTH = 2;
+	static final int EAST = 1;
+	static final int WEST = 3;
+	static int answer=0;
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
@@ -37,55 +37,91 @@ public class Main {
 				map[i][j] = Integer.parseInt(input[j]);
 		}
 		
-//		현재 위치를 청소한다.
-//		현재 위치에서 현재 방향을 기준으로 왼쪽방향부터 차례대로 탐색을 진행한다.
-//		왼쪽 방향에 아직 청소하지 않은 공간이 존재한다면, 그 방향으로 회전한 다음 한 칸을 전진하고 1번부터 진행한다.
-//		왼쪽 방향에 청소할 공간이 없다면, 그 방향으로 회전하고 2번으로 돌아간다.
-//		네 방향 모두 청소가 이미 되어있거나 벽인 경우에는, 바라보는 방향을 유지한 채로 한 칸 후진을 하고 2번으로 돌아간다.
-//		네 방향 모두 청소가 이미 되어있거나 벽이면서, 뒤쪽 방향이 벽이라 후진도 할 수 없는 경우에는 작동을 멈춘다.
-		
 		//0 : 북쪽, 1 : 동쪽, 2: 남쪽, 3 : 서쪽
-		while(true) {
-			
-			boolean flag=false;
+		whileloop: while(r >=0 && c >=0 && r < n && c < m) {
 			map[r][c]=-1; // 현재 위치를 청소
-			
-			loop : for(int i=0;i<4;i++) {
+			forloop : for(int i=0;i<=4;i++) {
 				
-				if(i!=0)
-					d = (d==0)?3:(--d);
-				
-				switch(d) {
-				//북	
-				case 0->{
-					if(isInTheMap(r-1, c) && map[r-1][c]==0) {
-						--r;
-						flag=true;
-						break loop;
+				if(i==4) {
+					switch(d) {
+					case EAST->{
+						if(map[r][c-1]==1) {
+							break whileloop;
+						}
+						c--;
+					}
+					case WEST->{
+						if(map[r][c+1]==1) {
+							break whileloop;
+						}
+						c++;
+					}
+					case SOUTH->{
+						if(map[r-1][c]==1) {
+							break whileloop;
+						}
+						r--;
+					}
+					case NORTH->{
+						if(map[r+1][c]==1) {
+							break whileloop;
+						}
+						r++;
 					}
 				}
-				//서
-				case 3->{
-					
-				}
-				//남
-				case 2->{
-					
-				}
-				//동
-				case 1->{
-					
+					continue;
 				}
 				
+				switch(d) {
+					case EAST->{
+						d = NORTH;
+						if(map[r-1][c]==0) {
+							r--;
+							break forloop;
+						}else continue;
+						
+					}
+					case WEST->{
+						d = SOUTH;
+						if(map[r+1][c]==0) {
+							r++;
+							break forloop;
+						}else continue;
+						
+					}
+					case SOUTH->{
+						d = EAST;
+						if(map[r][c+1]==0) {
+							c++;
+							break forloop;
+						}else continue;
+						
+					}
+					case NORTH->{
+						d = WEST;
+						if(map[r][c-1]==0) {
+							c--;
+							break forloop;
+						}else continue;
+						
+					}
 				}
 			}
-			if(flag)
-				continue;
 			
-			if(d==0)
-				break;
+			
 		}
 		
+		for(int i=0;i<n;i++) {
+			for(int j=0;j<m;j++) {
+//				System.out.print(map[i][j] + "\t");
+				if(map[i][j]==-1)
+					answer++;
+			}
+//			System.out.println();
+		}
+			
+				
+		bw.write(answer+"\n");
 		bw.close();
 		br.close();
 	}
