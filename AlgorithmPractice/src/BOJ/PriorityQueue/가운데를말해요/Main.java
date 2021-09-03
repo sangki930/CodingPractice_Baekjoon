ede04a01315b0eb35150660167211a2f70c4adde
sangkipark <sangki931@gmail.com>
2021-09-04 00:17:41 +0900

    커밋

--- AlgorithmPractice/src/BOJ/PriorityQueue/가운데를말해요/Main.java
+++ AlgorithmPractice/src/BOJ/PriorityQueue/가운데를말해요/Main.java
@@ -13,44 +13,27 @@ public class Main {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
-		PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a,b)->b-a);
-		PriorityQueue<Integer> minHeap = new PriorityQueue<>((a,b)->a-b);
-		
-		for(int i=0;i<n;i++) {
-			int res=-11111;
-			int val = Integer.parseInt(br.readLine());
-			if(i%2==0) {
-				
-				if(maxHeap.isEmpty() && minHeap.isEmpty()) {
-					res=val;
-					maxHeap.offer(res);
-				}else {
-					
-					if(minHeap.peek()>=val) maxHeap.offer(val);
-					else maxHeap.offer(val);
-					res = Math.min(minHeap.peek()!=null?minHeap.peek():11111, maxHeap.peek()!=null?maxHeap.peek():11111);
-				}
-//				System.out.println(maxHeap);
-//				System.out.println(minHeap);
-			}else {
-				if(minHeap.isEmpty()) {
-					res = Math.min(val, maxHeap.peek());
-					if(maxHeap.peek()>val) minHeap.offer(minHeap.poll());
-					else minHeap.offer(val);
-				}else {
-					if(minHeap.peek()<=val) {
-						minHeap.offer(val);
-					}else{
-						maxHeap.offer(val);
-					}
-					
-					res = Math.min(minHeap.peek()!=null?minHeap.peek():11111, maxHeap.peek()!=null?maxHeap.peek():11111);
-				}
-//				System.out.println(maxHeap);
-//				System.out.println(minHeap);
-			}
-			System.out.println(res);
-		}
+		StringBuilder sb = new StringBuilder();
+
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>((o1, o2) -> o1 - o2);
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((o1, o2) -> o2 - o1);
+
+        for(int i = 0 ; i < n; i++){
+            int num = Integer.parseInt(br.readLine());
+
+            if(minHeap.size() == maxHeap.size()) maxHeap.offer(num);
+            else minHeap.offer(num);
+
+            if(!minHeap.isEmpty() && !maxHeap.isEmpty())
+                if(minHeap.peek() < maxHeap.peek()){
+                    int tmp = minHeap.poll();
+                    minHeap.offer(maxHeap.poll());
+                    maxHeap.offer(tmp);
+                }
+
+            sb.append(maxHeap.peek() + "\n");
+        }
+        System.out.println(sb);
 		
 		br.close();
 	}
