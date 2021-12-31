970aa139a83d099e0c4ee551b78fddd5378b27eb
sangkipark <sangki931@gmail.com>
2021-12-31 23:51:54 +0900

    commit

---
+++ AlgorithmPractice/src/BOJ/bfs/아기상어/Main.java
@@ -0,0 +1,54 @@
+package BOJ.bfs.아기상어;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+
+class Point{
+	int x,y;
+}
+
+public class Main {
+
+	static int[] dx = {-1,0,1,0}, dy = {0,-1,0,1};
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		int[][] map = new int[n][n];
+		int s_x=-1,s_y=-1;
+		for(int i=0;i<n;i++) {
+			String[] input = br.readLine().split(" ");
+			for(int j=0;j<n;j++) {
+				map[i][j]=Integer.parseInt(input[j]);
+				if(map[i][j]==9) {
+					s_x=j;s_y=i;
+				}
+			}
+				
+		}
+		
+		LinkedList<Point> queue = new LinkedList<>();
+		queue.offer(new Point());
+		
+		int a=-2,b=-2;
+		
+		PriorityQueue<Point> pq = new PriorityQueue<>((x,y)->{
+			if(Math.abs(x.x-a)+Math.abs(x.y-b)==Math.abs(y.x-a)+Math.abs(y.y-b)) {
+				return -1;
+			}
+			return 1;
+		});
+		
+		while(!queue.isEmpty()) {
+			Point point = queue.poll();
+			
+		}
+		
+		br.close();
+	}
+
+}
