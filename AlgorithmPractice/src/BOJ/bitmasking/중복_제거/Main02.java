85e2b88ba4ab3af1d30f81258aa5e0dc94f1f69b
sangkipark <sangki931@gmail.com>
2022-02-02 22:10:18 +0900

    commit 중복제거

---
+++ AlgorithmPractice/src/BOJ/bitmasking/중복_제거/Main02.java
@@ -0,0 +1,30 @@
+package BOJ.bitmasking.중복_제거;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// 비트 마스킹
+// 출처 : 백준
+public class Main02 {
+
+	static final int MAX = 33554432;
+	static byte[] arr = new byte[MAX/8+1];
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		int a = 0;
+		while(st.hasMoreTokens()) {
+			a = Integer.parseInt(st.nextToken());
+			if((arr[a/8]& 1<<(a%8))>=1) {
+				continue;
+			}else {
+				arr[a/8] |= 1<<(a%8);
+				sb.append(a).append(" ");
+			}
+		}
+		System.out.println(sb);
+	}
+
+}
