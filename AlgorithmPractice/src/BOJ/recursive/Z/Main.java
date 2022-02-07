f426502d39e783077988326b68e464fb669ef11f
sangkipark <sangki931@gmail.com>
2022-02-07 23:22:06 +0900

    commit

--- AlgorithmPractice/src/BOJ/recursive/Z/Main.java
+++ AlgorithmPractice/src/BOJ/recursive/Z/Main.java
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 
 public class Main {
 
-	static int answer=-1;
+	static int cnt=-1,answer=-1;
 	static int r,c;
 	static boolean flag=false;
 	public static void main(String[] args) throws IOException {
@@ -16,29 +16,41 @@ public class Main {
 		int n = Integer.parseInt(input[0]);
 		r = Integer.parseInt(input[1]); c = Integer.parseInt(input[2]);
 		func(n,0,0);
-		System.out.println(answer);
+		System.out.println(cnt);
 		br.close();
 	}
 	
 	public static void func(int n, int row, int col) {
-		
+		System.out.println("n : "+n+", row : "+row+", col : "+col);
+		System.out.println(answer);
 		if(n==0)
 		{
 //			System.out.println(row+" "+col);
 			answer++;
 			if(row==r && col==c) {
-				flag=true; 
+				cnt=answer;
 			}
 			return;
 		}
-		
-		func(n-1,row,col);
-		if(flag) return ;
+		System.out.println("확인1");
+		if(r>=row && r<row+Math.pow(2, n-1) && c>=col && c<col+Math.pow(2, n-1)) {
+//			System.out.println("확인");
+			func(n-1,row,col);
+		}
+		else answer+=Math.pow(2, 2*n-2);
+		System.out.println("확인2");
+		if(r>=row && r<row+Math.pow(2, n-1) && c>=col+(int)Math.pow(2, n-1) && c<Math.pow(2, n))
 		func(n-1,row,col+(int)Math.pow(2, n-1));
-		if(flag) return ;
+		else answer+=Math.pow(2, 2*n-2);
+		System.out.println("확인3");
+		if(r>=row+(int)Math.pow(2, n-1) && r<row+Math.pow(2, n) && c>=col && c<Math.pow(2, n-1))
 		func(n-1,row+(int)Math.pow(2, n-1),col);
-		if(flag) return ;
+		else answer+=Math.pow(2, 2*n-2);
+		System.out.println("확인4");
+		if(r>=row+(int)Math.pow(2, n-1) && r<row+Math.pow(2, n) && c>=col+(int)Math.pow(2, n-1) && c<Math.pow(2, n))
 		func(n-1,row+(int)Math.pow(2, n-1),col+(int)Math.pow(2, n-1));
+		else answer+=Math.pow(2, 2*n-2);
+		cnt=answer;
 	}
 
 }
