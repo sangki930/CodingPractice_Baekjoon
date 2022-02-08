ed69adda3c9c4e24687a56ed467b21d6f5b3725e
sangkipark <sangki931@gmail.com>
2022-02-09 01:04:06 +0900

    commit Z

--- AlgorithmPractice/src/BOJ/recursive/Z/Main.java
+++ AlgorithmPractice/src/BOJ/recursive/Z/Main.java
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 
 public class Main {
 
-	static int cnt=-1,answer=-1;
+	static int cnt=-1,answer=0;
 	static int r,c;
 	static boolean flag=false;
 	public static void main(String[] args) throws IOException {
@@ -16,41 +16,22 @@ public class Main {
 		int n = Integer.parseInt(input[0]);
 		r = Integer.parseInt(input[1]); c = Integer.parseInt(input[2]);
 		func(n,0,0);
-		System.out.println(cnt);
 		br.close();
 	}
 	
 	public static void func(int n, int row, int col) {
-		System.out.println("n : "+n+", row : "+row+", col : "+col);
-		System.out.println(answer);
-		if(n==0)
-		{
-//			System.out.println(row+" "+col);
-			answer++;
-			if(row==r && col==c) {
-				cnt=answer;
-			}
+		if(row==r && col==c) {
+			System.out.println(answer);
 			return;
 		}
-		System.out.println("확인1");
-		if(r>=row && r<row+Math.pow(2, n-1) && c>=col && c<col+Math.pow(2, n-1)) {
-//			System.out.println("확인");
+		
+		if(r<row+(int)Math.pow(2,n) && r>=row && c<col+(int)Math.pow(2,n) && c>=col ) {
 			func(n-1,row,col);
+			func(n-1,row,col+(int)Math.pow(2, n-1));
+			func(n-1,row+(int)Math.pow(2, n-1),col);
+			func(n-1,row+(int)Math.pow(2, n-1),col+(int)Math.pow(2, n-1));
+		}else {
+			answer+=Math.pow(2, 2*n);
 		}
-		else answer+=Math.pow(2, 2*n-2);
-		System.out.println("확인2");
-		if(r>=row && r<row+Math.pow(2, n-1) && c>=col+(int)Math.pow(2, n-1) && c<Math.pow(2, n))
-		func(n-1,row,col+(int)Math.pow(2, n-1));
-		else answer+=Math.pow(2, 2*n-2);
-		System.out.println("확인3");
-		if(r>=row+(int)Math.pow(2, n-1) && r<row+Math.pow(2, n) && c>=col && c<Math.pow(2, n-1))
-		func(n-1,row+(int)Math.pow(2, n-1),col);
-		else answer+=Math.pow(2, 2*n-2);
-		System.out.println("확인4");
-		if(r>=row+(int)Math.pow(2, n-1) && r<row+Math.pow(2, n) && c>=col+(int)Math.pow(2, n-1) && c<Math.pow(2, n))
-		func(n-1,row+(int)Math.pow(2, n-1),col+(int)Math.pow(2, n-1));
-		else answer+=Math.pow(2, 2*n-2);
-		cnt=answer;
 	}
-
 }
