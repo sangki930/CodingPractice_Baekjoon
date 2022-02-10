2c92efa72af536eba0d0a84090d5a2e15177e4e6
sangkipark <sangki931@gmail.com>
2022-02-11 00:36:56 +0900

    숨바꼭질 2번 커밋

--- AlgorithmPractice/src/BOJ/bfs/숨바꼭질_02/Main.java
+++ AlgorithmPractice/src/BOJ/bfs/숨바꼭질_02/Main.java
@@ -1,68 +1,61 @@
 package BOJ.bfs.¼û¹Ù²ÀÁú_02;
 
 import java.util.LinkedList;
-import java.util.Map;
 import java.util.Queue;
 import java.util.Scanner;
-import java.util.TreeMap;
 
 public class Main {
-    static int N;
-    static int K;
     static int[] check = new int[100001];
-    static int answer=0;
-    static Map<Long,Long> map = new TreeMap<>((a,b)->Long.compare(a, b));
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        K = sc.nextInt();
-
-        if (N == K) {
-            System.out.println(0+"\n"+1);
-            return ;
-        }
-        else {
-            bfs(N);
-        }
-        Long key = !map.keySet().isEmpty()?map.keySet().iterator().next():0L;
-        System.out.print(key+"\n"+map.getOrDefault(key,(long) 1));
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+        System.out.println(bfs(N,K));
     }
 
-    static void bfs(int num) {
+    static String bfs(int num, int K) {
+    	
+    	if(num>=K) {
+    		return (num-K)+"\n"+1;
+    	}
+    	
         Queue<Integer> q = new LinkedList<>();
         q.add(num);
         check[num] = 1;
-
+        int minTime = Integer.MAX_VALUE,cnt=0;
+        
         while (!q.isEmpty()) {
-            int temp = q.poll();
+            int now = q.poll();
 
+            if(minTime<check[now]) break;
+            
             for (int i = 0; i < 3; i++) {
                 int next;
 
                 if (i == 0) {
-                    next = temp + 1;
+                    next = now + 1;
                 } else if (i == 1) {
-                    next = temp - 1;
+                    next = now - 1;
                 } else {
-                    next = temp * 2;
+                    next = now * 2;
                 }
+                
+                if(next >= 0 && next < check.length) {
+                    if (next == K) {
+//                      System.out.println(check[now]);
+                    	minTime = check[now];
+                      cnt++;
+                  }
 
-                if (next == K) {
-                    map.put((long)check[temp], map.getOrDefault((long)check[temp], 0L)+1L);
-                    continue;
-                }
-
-                if (next >= 0 && next < check.length) {
-                		
-                	if(check[next]==0) {
-                        check[next] = check[temp] + 1;
-                        q.add(next);
-                	}else if(check[next]>0 && map.containsKey((long)check[next])) {
-                		map.put((long)check[temp], map.getOrDefault((long)check[temp], 0L)+1L);
-//                		q.add(next);
-                	} 
+                  if (check[next] == 0 || check[next]==check[now]+1) {
+                      q.add(next);
+                      check[next] = check[now] + 1;
+                  }
                 }
             }
         }
+        
+        return minTime+"\n"+cnt;
     }
 }
