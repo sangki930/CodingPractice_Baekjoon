369318befc0789001cd1aa4b34a536999c331208
sangkipark <sangki931@gmail.com>
2022-02-27 11:28:05 +0900

    commit 수열

---
+++ AlgorithmPractice/src/BOJ/prefixSum/수열/Main.java
@@ -0,0 +1,18 @@
+package BOJ.prefixSum.¼ö¿­;
+
+import java.io.*;
+import java.util.Arrays;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		
+		
+		br.close();
+
+	}
+
+}
