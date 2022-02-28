6637d2896c41a1ab6db00261482c845176f0ee84
sangkipark <sangki931@gmail.com>
2022-02-28 22:06:27 +0900

    commit

--- AlgorithmPractice/src/BOJ/prefixSum/수열/Main.java
+++ AlgorithmPractice/src/BOJ/prefixSum/수열/Main.java
@@ -1,7 +1,8 @@
 package BOJ.prefixSum.¼ö¿­;
 
-import java.io.*;
-import java.util.Arrays;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
 
@@ -9,8 +10,17 @@ public class Main {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		
-		
+		String[] input = br.readLine().split(" ");
+		int n = Integer.parseInt(input[0]), k = Integer.parseInt(input[1]),answer=Integer.MIN_VALUE;
+		int[] arr = new int[n+1];
+		input = br.readLine().split(" ");
+		for(int i=1;i<=n;i++) {
+			arr[i] = arr[i-1]+Integer.parseInt(input[i-1]);
+		}
+		for(int i=k;i<=n;i++) {
+			answer = Math.max(answer, arr[i]-arr[i-k]);
+		}
+		System.out.println(answer);
 		br.close();
 
 	}
