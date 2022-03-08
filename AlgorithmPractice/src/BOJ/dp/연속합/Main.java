30fc1262583adc658933cbc0aa2130272c6fc875
sangkipark <sangki931@gmail.com>
2022-03-08 13:27:12 +0900

    연속합

---
+++ AlgorithmPractice/src/BOJ/dp/연속합/Main.java
@@ -0,0 +1,30 @@
+package BOJ.dp.¿¬¼ÓÇÕ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+ 
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int answer=0;
+        
+        int[] arr = new int[n], dp = new int[n];
+        String[] input = br.readLine().split(" ");
+        for(int i=0;i<n;i++)
+        	arr[i]=Integer.parseInt(input[i]);
+        dp[0]=arr[0];
+        answer = dp[0];
+        
+        for (int i = 1; i < n; i++) {
+            dp[i] = Math.max(dp[i-1] + arr[i], arr[i]);
+            answer = Math.max(answer, dp[i]);
+        }
+        
+        System.out.println(answer);
+        
+        br.close();
+    }
+}
