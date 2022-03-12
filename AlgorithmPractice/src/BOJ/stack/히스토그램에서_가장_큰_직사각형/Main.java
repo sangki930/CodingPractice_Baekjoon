65a032d26ccb69c244b1a4eb7db29b50fe221b83
sangkipark <sangki931@gmail.com>
2022-03-12 23:34:24 +0900

    commit

--- AlgorithmPractice/src/BOJ/stack/히스토그램에서_가장_큰_직사각형/Main.java
+++ AlgorithmPractice/src/BOJ/stack/히스토그램에서_가장_큰_직사각형/Main.java
@@ -10,7 +10,6 @@ import java.util.StringTokenizer;
 public class Main {
 	
 	public static int[] histogram;
-	
 	public static void main(String[] args) throws IOException {
 	
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
