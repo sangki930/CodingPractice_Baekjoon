ba3ee22851cbcbf72d88e8ebdd093011cd2d335a
sangkipark <sangki931@gmail.com>
2022-03-17 22:23:54 +0900

    commit 직사각형과 쿼리

---
+++ AlgorithmPractice/src/BOJ/prefixSum/직사각형과_쿼리/Main.java
@@ -0,0 +1,18 @@
+package BOJ.prefixSum.직사각형과_쿼리;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		
+		br.close();
+
+	}
+
+}
