95c1a7bee3fda55e0a38ac5314e4c51ea4a95865
sangkipark <sangki931@gmail.com>
2022-03-26 23:33:22 +0900

    asdas

--- AlgorithmPractice/src/BOJ/MST/최소스패닝트리/Main.java
+++ AlgorithmPractice/src/BOJ/MST/최소스패닝트리/Main.java
@@ -19,7 +19,6 @@ class Edge {
 
 public class Main {
     public static int[] parent;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
