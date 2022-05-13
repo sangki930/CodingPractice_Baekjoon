e83a461d692486b7aaa4d6fb19da027c1d3b2dda
sangkipark <sangki931@gmail.com>
2022-05-13 23:40:39 +0900

    commit 동전1

---
+++ AlgorithmPractice/src/BOJ/dp/동전1/Main.java
@@ -0,0 +1,28 @@
+package BOJ.dp.µ¿Àü1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+ 
+public class Main {
+ 
+	public static void main(String[] args) throws IOException {
+ 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String[] input = br.readLine().split(" ");
+		int n = Integer.parseInt(input[0]), k = Integer.parseInt(input[1]);
+		int[] arr = new int[n+2];
+		int[] dp = new int[k+2];
+		dp[0]=1;
+		for(int i=1;i<=n;i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+			for(int j=arr[i];j<=k;j++) {
+				dp[j]+=dp[j-arr[i]];
+			}
+		}
+		System.out.println(dp[k]);
+		br.close();
+	}
+ 
+}
