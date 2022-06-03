0c1dd81c20f644edc977d33ba74b2fa49bc96029
sangkipark <sangki931@gmail.com>
2022-06-03 23:07:47 +0900

    commit

--- AlgorithmPractice/src/BOJ/PriorityQueue/N번째큰수/Main.java
+++ AlgorithmPractice/src/BOJ/PriorityQueue/N번째큰수/Main.java
@@ -1,30 +1,29 @@
 package BOJ.PriorityQueue.N번째큰수;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.PriorityQueue;
+import java.util.Scanner;
 
 
 public class Main {
 
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args){
 		// TODO Auto-generated method stub
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
 		PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)->Integer.compare(a,b));
 		
 		for(int i=0;i<n;i++) {
-			String[] tmp = br.readLine().split(" ");
-			for(int j=0;j<tmp.length;j++) {
-				pq.offer(Integer.parseInt(tmp[j]));
+//			String[] tmp = br.readLine().split(" ");
+			for(int j=0;j<n;j++) {
+				pq.offer(sc.nextInt());
 				if(pq.size()>n) pq.poll();
 			}
 		}
 //		System.out.println(pq);
 		System.out.println(pq.poll());
 		
-		br.close();
+		sc.close();
 	}
 
 }
