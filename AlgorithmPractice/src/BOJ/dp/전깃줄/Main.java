8c62e0236c6f1ad2f0565547a60251b18c9196c9
sangkipark <sangki931@gmail.com>
2022-06-04 22:31:48 +0900

    commit 가장 긴 감소하는 수열

---
+++ AlgorithmPractice/src/BOJ/dp/전깃줄/Main.java
@@ -0,0 +1,18 @@
+package BOJ.dp.ภüฑ๊มู;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String[] tmp = br.readLine().split(" ");
+		
+		
+        br.close();
+	}
+
+}
