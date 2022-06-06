a9bb6d73d3f78d9e7a350e41aab197aef78be5db
sangkipark <sangki931@gmail.com>
2022-06-06 19:55:51 +0900

    commit 쿼드트리

---
+++ AlgorithmPractice/src/BOJ/divideAndConquer/쿼드트리/Main.java
@@ -0,0 +1,17 @@
+package BOJ.divideAndConquer.ÄõµåÆ®¸®;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		
+		br.close();
+	}
+
+}
