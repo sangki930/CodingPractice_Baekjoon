926f17856428256202d46da0d169dae0c33edba6
sangkipark <sangki931@gmail.com>
2022-06-16 22:18:38 +0900

    commit

--- AlgorithmPractice/src/BOJ/greedy/주식/Main.java
+++ AlgorithmPractice/src/BOJ/greedy/주식/Main.java
@@ -9,7 +9,6 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
 		int t = Integer.parseInt(br.readLine());
 		
 		for(int i=0;i<t;i++) {
