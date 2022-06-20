19124d9013a7e5427a867b67d130ffb844b36870
sangkipark <sangki931@gmail.com>
2022-06-20 23:50:19 +0900

    commit 팩토피얼0의 개수

---
+++ AlgorithmPractice/src/BOJ/math/팩토리얼0의개수/Main.java
@@ -0,0 +1,23 @@
+package BOJ.math.팩토리얼0의개수;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		bw.write(((n/5)+(n/25)+(n/125))+"");
+		bw.close();
+		br.close();
+	}
+
+}
