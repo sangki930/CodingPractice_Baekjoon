1a1512f7bbc9c24bb61b80bedc031370db6e8966
sangkipark <sangki931@gmail.com>
2022-06-23 23:25:26 +0900

    commit 컵홀더(미완)

---
+++ AlgorithmPractice/src/BOJ/greedy/컵홀더/Main.java
@@ -0,0 +1,22 @@
+package BOJ.greedy.ÄÅÈ¦´õ;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+// 2810¹ø / ÄÅÈ¦´õ / ºê·ÐÁî 1
+public class Main {
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        br.close();
+        bw.close();
+    }
+
+}
