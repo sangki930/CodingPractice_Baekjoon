cd2ab23f545069d505caebf287cbd5cb3c84de84
sangkipark <sangki931@gmail.com>
2022-07-17 23:06:26 +0900

    commit 가장 큰 정사각형

---
+++ AlgorithmPractice/src/BOJ/dp/가장큰정사각형/Main.java
@@ -0,0 +1,13 @@
+package BOJ.dp.가장큰정사각형;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+ 
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        br.close();
+    }
+}
