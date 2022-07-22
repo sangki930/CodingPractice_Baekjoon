aa859088b680224a122f31c67182fbaa4bca808b
USER <USER@AISELN-15U50P>
2022-07-22 21:48:41 +0900

    test commit

--- AlgorithmPractice/src/BOJ/bfs/구슬_탈출/Main.java
+++ AlgorithmPractice/src/BOJ/bfs/구슬_탈출/Main.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+//[13459] ±∏ΩΩ≈ª√‚2 (∞ÒµÂ2)
 public class Main {
 
 	public static void main(String[] args) throws IOException {
