9111fc070770c5a810d6e0a7fac91a2c16cdef79
sangkipark <sangki931@gmail.com>
2022-07-30 20:52:41 +0900

    commit 완전이진트리

--- AlgorithmPractice/src/BOJ/tree/완전이진트리/Main.java
+++ AlgorithmPractice/src/BOJ/tree/완전이진트리/Main.java
@@ -3,52 +3,51 @@ package BOJ.tree.완전이진트리;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
-class Node{
-	
-	int id;
-	int no;
-	int level;
-	Node left;
-	Node right;
-	/**
-	 * @param id
-	 * @param no
-	 * @param level
-	 * @param left
-	 * @param right
-	 */
-	public Node(int id, int no, int level, Node left, Node right) {
-		this.id = id;
-		this.no = no;
-		this.level = level;
-		this.left = left;
-		this.right = right;
-	}
-	
-}
 
 public class Main {
 
-	static Node[] nodes;
-	static int k,num_node;
+	static int k,num_node,idx;
+	static int[] arr;
+	static boolean[] visited;
+	static ArrayList<Integer>[] tree;
 	public static void main(String[] args) throws IOException{
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		k = Integer.parseInt(br.readLine());
 		num_node = (int)Math.pow(2, k)-1;
-		nodes = new Node[num_node];
 		
+		String[] input = br.readLine().split(" ");
+		arr = new int[input.length];
+		for(int i=0;i<input.length;i++) {
+			arr[i] = Integer.parseInt(input[i]);
+		}
+		tree = new ArrayList[k];
+		for(int i=0;i<k;i++) {
+			tree[i] = new ArrayList<>();
+		}
+		searchTree(1,0);
+		StringBuilder sb = new StringBuilder();
 		
+		for(int i=0;i<k;i++) {
+			for(int j : tree[i])
+				sb.append(j).append(" ");
+			sb.append("\n");
+		}
+		System.out.println(sb.toString().trim());
 		br.close();
 	}
 	
-	public static Node makeTree(int id,int level) {
-		if(level==k) return null;
-		if(nodes[id]==null) nodes[id] = new Node(id+1,-1,(int)(Math.log(id+1)/Math.log(2))+1,null,null);
-		nodes[id].left = makeTree(id*2,level+1);
-		nodes[id].right = makeTree(id*2+1,level+1);
-		return nodes[id];
+	public static int searchTree(int id, int level) {
+		if(level==k-1) {
+			tree[level].add(arr[idx++]);
+			return id;
+		}
+		searchTree(id*2,level+1);
+		tree[level].add(arr[idx++]);
+		searchTree(id*2+1,level+1);
+		return id;
 	}
 
 }
