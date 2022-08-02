3ee80a1b62ea8dee113700472c5f12b0cc656536
sangkipark <sangki931@gmail.com>
2022-08-02 23:57:22 +0900

    트리만들기 14244

--- AlgorithmPractice/src/BOJ/tree/트리만들기_14244/Main.java
+++ AlgorithmPractice/src/BOJ/tree/트리만들기_14244/Main.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+// 트리만들기
 public class Main {
 
 	public static void main(String[] args) throws IOException{
