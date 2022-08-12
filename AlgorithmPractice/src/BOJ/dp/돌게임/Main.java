a081428e5392111cf9d171c25e72dcbe9b0be349
USER <USER@AISELN-15U50P>
2022-08-12 15:16:41 +0900

    commit 돌게임

---
+++ AlgorithmPractice/src/BOJ/dp/돌게임/Main.java
@@ -0,0 +1,24 @@
+package BOJ.dp.µπ∞‘¿”;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int n = Integer.parseInt(br.readLine());
+		String answer = "";
+		if(n%2==1)
+			answer = "SK";
+		else
+			answer = "CY";
+		
+		System.out.println(answer);
+		br.close();
+	}
+
+}
