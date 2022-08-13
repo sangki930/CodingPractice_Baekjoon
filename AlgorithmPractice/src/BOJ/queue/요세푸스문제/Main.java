ca4d9572856b9812398eccd5baf61cd1c6e39a49
sangkipark <sangki931@gmail.com>
2022-08-13 23:04:56 +0900

    commit 요세푸스문제

---
+++ AlgorithmPractice/src/BOJ/queue/요세푸스문제/Main.java
@@ -0,0 +1,14 @@
+package BOJ.queue.요세푸스문제;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// 요세푸스 문제 - 실버 4
+public class Main{
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		br.close();
+	}
+}
