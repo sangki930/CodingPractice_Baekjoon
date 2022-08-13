3242e61bfe7ce11b3f5570dcfae4c460e1ecda86
sangkipark <sangki931@gmail.com>
2022-08-13 23:23:47 +0900

    commit 요세푸스문제

--- AlgorithmPractice/src/BOJ/queue/요세푸스문제/Main.java
+++ AlgorithmPractice/src/BOJ/queue/요세푸스문제/Main.java
@@ -3,11 +3,32 @@ package BOJ.queue.요세푸스문제;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 // 요세푸스 문제 - 실버 4
 public class Main{
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String[] input = br.readLine().split(" ");
+		
+		int n = Integer.parseInt(input[0]), k = Integer.parseInt(input[1]);
+		
+		LinkedList<Integer> circular_list = new LinkedList<>();
+		for(int i=0;i<n;i++)
+			circular_list.add(i+1);
+		List<String> answer = new ArrayList<>();
+		int popNum = 0;
+		
+		while(!circular_list.isEmpty()) {
+			popNum = (popNum + (k-1)) % circular_list.size();
+			int popElement = circular_list.remove(popNum);
+			answer.add(popElement+"");
+		}
+		
+		System.out.printf("<%s>",String.join(", ", answer));
 		
 		br.close();
 	}
