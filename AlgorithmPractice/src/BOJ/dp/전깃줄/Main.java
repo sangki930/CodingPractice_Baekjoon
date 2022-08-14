c55ad36d09b7e61cc28f0acbc8acaee849e7f92c
sangkipark <sangki931@gmail.com>
2022-08-14 22:08:01 +0900

    commit 전깃줄

--- AlgorithmPractice/src/BOJ/dp/전깃줄/Main.java
+++ AlgorithmPractice/src/BOJ/dp/전깃줄/Main.java
@@ -3,13 +3,13 @@ package BOJ.dp.ภüฑ๊มู;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		String[] tmp = br.readLine().split(" ");
 		
 		
         br.close();
