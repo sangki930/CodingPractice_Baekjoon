1578448cf9877934f99f83e424cd8eab1a1481f0
sangkipark <sangki931@gmail.com>
2022-11-19 23:10:06 +0900

    commit 스택

--- AlgorithmPractice/src/BOJ/stack/스택/Main.java
+++ AlgorithmPractice/src/BOJ/stack/스택/Main.java
@@ -3,6 +3,7 @@ package BOJ.stack.Ω∫≈√;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Stack;
 
 public class Main {
 
@@ -11,6 +12,32 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		int n = Integer.parseInt(br.readLine());
+		Stack<String> stack = new Stack<>();
+		StringBuilder sb = new StringBuilder();
+		for(int i=0;i<n;i++) {
+			String[] input = br.readLine().split(" ");
+			String res = "";
+			switch(input[0]) {
+				case "push"->{
+					stack.push(input[1]);
+					continue;
+				}
+				case "pop"->{
+					res = stack.isEmpty()?"-1":stack.pop();
+				}
+				case "size"->{
+					res = String.valueOf(stack.size());
+				}
+				case "empty"->{
+					res = stack.isEmpty()?"1":"0";
+				}
+				case "top"->{
+					res = stack.isEmpty()?"-1":stack.peek();
+				}
+			}
+			sb.append(res).append("\n");
+		}
+		System.out.println(sb);
 		
 		br.close();
 	}
