faa7515d137d02c4d01dec3a00c434accceb687e
sangkipark <sangki931@gmail.com>
2022-12-05 22:23:39 +0900

    commit 전화번호목록

--- AlgorithmPractice/src/BOJ/tree/trie/전화번호목록/Main.java
+++ AlgorithmPractice/src/BOJ/tree/trie/전화번호목록/Main.java
@@ -3,40 +3,35 @@ package BOJ.tree.trie.전화번호목록;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Map;
-
-class Data{
-	int level;
-	char c;
-	public Data(int level, char c) {
-		super();
-		this.level = level;
-		this.c = c;
-	}
-	@Override
-	public String toString() {
-		return "Data [level=" + level + ", c=" + c + "]";
-	}
-}
+import java.util.Arrays;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException{
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
+		int t = Integer.parseInt(br.readLine());
 		
-		for(int i=0;i<n;i++) {
-			int t = Integer.parseInt(br.readLine());
-			Map<String,LinkedList<String>> tree = new HashMap<>();
-			tree.put("0,0", new LinkedList<>());
-			for(int j=1;j<=t;j++) {
-				LinkedList<String> nodes = (j!=1)?tree.getOrDefault(j, new LinkedList<>()):tree.getOrDefault(tree, new LinkedList<>());
-				
-			}
-		}
+		for(int i = 0; i < t; i++) {
+            int n = Integer.parseInt(br.readLine());
+            
+            String[] nums = new String[n];
+            for(int j = 0; j < n; j++) {
+                nums[j] = br.readLine();
+            }
+            Arrays.sort(nums);
+            
+            boolean isInclude = true;
+            for(int j = 1; j < n; j++) {
+                if(nums[j].startsWith(nums[j - 1])) {
+                    isInclude = false;
+                    break;
+                }
+            }
+            if(isInclude == false) System.out.println("NO");
+            else System.out.println("YES");
+        }
+		
 		br.close();
 	}
 
