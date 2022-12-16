1cc2a7cfa1d541db8da43497ce9b32962fca6b04
sangki930 <sangki931@gmail.com>
2022-12-16 10:16:01 +0900

    commit 점프

---
+++ AlgorithmPractice/src/BOJ/dp/점프/Main.java
@@ -0,0 +1,35 @@
+package BOJ.dp.มกวม;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		int[][] board = new int[n][n];
+		long[][] cnts = new long[n][n];
+		for(int i=0;i<n;i++) {
+			board[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+		}
+		cnts[0][0]=1;
+		
+		for(int i=0;i<n;i++) {
+			for(int j=0;j<n;j++) {
+				if(cnts[i][j]==0 || (i==n-1 && j==n-1) ) continue;
+				int v = board[i][j];
+				int d = i+v, r = j+v;
+				if(0<=r && r<n) cnts[i][r] += cnts[i][j];
+				if(0<=d && d<n) cnts[d][j] += cnts[i][j];
+			}
+		}
+		System.out.println(cnts[n-1][n-1]);
+		br.close();
+	}
+
+}
