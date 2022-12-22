1942232858232b7b7ddd4771231c44dfbbe9a3a5
sangki930 <sangki931@gmail.com>
2022-12-22 13:07:41 +0900

    commit 문자열집합

---
+++ AlgorithmPractice/src/BOJ/hash/문자열집합/Main.java
@@ -0,0 +1,30 @@
+package BOJ.hash.문자열집합;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br =  new BufferedReader(new InputStreamReader(System.in));
+		String[] tmp = br.readLine().split(" ");
+		int n = Integer.parseInt(tmp[0]);
+		int m = Integer.parseInt(tmp[1]);
+		int answer = 0;
+		Set<String> set = new HashSet<>();
+		for(int i=0;i<n;i++)
+			set.add(br.readLine());
+		for(int i=0;i<m;i++)
+			if(set.contains(br.readLine()))
+				answer++;
+		System.out.println(answer);
+		br.close();
+
+	}
+
+}
