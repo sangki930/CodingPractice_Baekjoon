82523276819f1135515f25c0c998eae0c15d2cf4
sangkipark <sangki931@gmail.com>
2023-01-12 23:55:07 +0900

    commit

--- AlgorithmPractice/src/BOJ/implementation/주사위굴리기/Main.java
+++ AlgorithmPractice/src/BOJ/implementation/주사위굴리기/Main.java
@@ -8,7 +8,7 @@ import java.util.Arrays;
 public class Main {
 
 	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
+	
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		String[] input = br.readLine().split(" ");
