a71d9a872c7708e8991f13d52b397b5d56e23f16
sangkipark <sangki931@gmail.com>
2023-02-04 02:35:08 +0900

    commit 가르침

---
+++ AlgorithmPractice/src/BOJ/bruteforce/가르침/Main.java
@@ -0,0 +1,72 @@
+package BOJ.bruteforce.가르침;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+ 
+    static int n, k;
+    static int max = Integer.MIN_VALUE;
+    static boolean[] visited;
+    static String[] word;
+    
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ 
+        String[] input = br.readLine().split(" ");
+        n = Integer.parseInt(input[0]);
+        k = Integer.parseInt(input[1]);
+        word = new String[n];
+        for(int i = 0; i < n; i++) {
+            String str = br.readLine();
+            str = str.replace("anta", "");
+            str = str.replace("tica", "");
+            word[i] = str;
+        }
+        
+        if(k < 5) { 
+            System.out.println("0");
+            return;
+        } else if(k == 26) {
+            System.out.println(n);
+            return;
+        }
+        
+        visited = new boolean[26];
+        visited['a' - 'a'] = true;
+        visited['c' - 'a'] = true;
+        visited['i' - 'a'] = true;
+        visited['n' - 'a'] = true;
+        visited['t' - 'a'] = true;
+        
+        backtracking(0, 0);
+        System.out.println(max);
+    }
+    
+    public static void backtracking(int alpha, int len) {
+        if(len == k - 5) {
+            int count = 0;
+            for(int i = 0; i < n; i++) { //뽑은 알파벳으로 몇개의 단어를 읽을 수 있는지 카운트.
+                boolean read = true;
+                for(int j = 0; j < word[i].length(); j++) {
+                    if(!visited[word[i].charAt(j) - 'a']) {
+                        read = false;
+                        break;
+                    }
+                }
+                if(read) count++;
+            }
+            max = Math.max(max, count);
+            return;
+        }
+        
+        for(int i = alpha; i < 26; i++) {
+            if(visited[i] == false) {
+                visited[i] = true;
+                backtracking(i, len + 1);
+                visited[i] = false;
+            }
+        }
+    }
+}
