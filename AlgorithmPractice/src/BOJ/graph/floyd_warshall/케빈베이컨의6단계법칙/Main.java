7b5e8f7b12252e8b7836319e2b7a25e28c78dd24
sangki930 <sangki931@gmail.com>
2023-02-24 16:50:48 +0900

    commit 과제

--- AlgorithmPractice/src/BOJ/graph/floyd_warshall/케빈베이컨의6단계법칙/Main.java
+++ AlgorithmPractice/src/BOJ/graph/floyd_warshall/케빈베이컨의6단계법칙/Main.java
@@ -6,46 +6,60 @@ import java.io.InputStreamReader;
 
 public class Main {
 
+	static int INF = 999999999;
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-		int m = Integer.parseInt(br.readLine());
-		int[][] arr = new int[n+1][n+1];
-		final int INF = (int)Math.pow(10,9);
-		for(int i=1;i<=n;i++)
-			for(int j=1;j<=n;j++)
-				if(i!=j)
-					arr[i][j]=INF;
-
-		for(int k=0;k<m;k++) {
-			String[] tmp = br.readLine().split(" ");
-			int i = Integer.parseInt(tmp[0]), j = Integer.parseInt(tmp[1]);
-			arr[i][j]=Math.min(Integer.parseInt(tmp[2]),arr[i][j]);
-		}
-
-		// 플로이드 와샬 알고리즘
-		for(int i=1;i<=n;i++) {
-			for(int j=1;j<=n;j++) {
-				for(int k=1;k<=n;k++) {
-					arr[j][k]=Math.min(arr[j][i]+arr[i][k], arr[j][k]);
-				}
-			}
-		}
-		
-		StringBuilder sb = new StringBuilder();
-		
-		for(int i=1;i<=n;i++) {
-			for(int j=1;j<=n;j++)
-				if(arr[i][j]>=INF)
-					sb.append("0 ");
-				else
-					sb.append(arr[i][j]+" ");
-			sb.append("\n");
-		}
-			
-		System.out.println(sb);
+		String[] tmp = br.readLine().split(" ");
+		int n = Integer.parseInt(tmp[0]);
+        int m = Integer.parseInt(tmp[1]);
+        int[][] arr = new int[n + 1][n + 1];
+ 
+        // 초기값 설정
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                arr[i][j] = INF;
+ 
+                if (i == j) {
+                    arr[i][j] = 0;
+                }
+            }
+        }
+        
+        for (int i = 0; i < m; i++) {
+        	tmp = br.readLine().split(" ");
+            int x = Integer.parseInt(tmp[0]);
+            int y = Integer.parseInt(tmp[1]);
+ 
+            arr[x][y] = arr[y][x] = 1;
+        }
+ 
+        for (int k = 1; k <= n; k++) {
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= n; j++) {
+                    
+                    if (arr[i][j] > arr[i][k] + arr[k][j]) {
+                        arr[i][j] = arr[i][k] + arr[k][j];
+                    }
+                }
+            }
+        }
+ 
+        int res = INF;
+        int idx = -1;
+ 
+        for (int i = 1; i <= n; i++) {
+            int total = 0;
+            for (int j = 1; j <= n; j++) {
+                total += arr[i][j];
+            }
+ 
+            if (res > total) {
+                res = total;
+                idx = i;
+            }
+        }
+        System.out.println(idx);
 		br.close();
 	}
-
 }
---
+++ AlgorithmPractice/src/BOJ/greedy/과제/Main.java
@@ -0,0 +1,74 @@
+package BOJ.greedy.과제;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+
+class Data{
+	int d,w;
+
+	public Data(int d, int w) {
+		this.d = d;
+		this.w = w;
+	}
+
+	@Override
+	public String toString() {
+		return "Data [d=" + d + ", w=" + w + "]";
+	}
+}
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int n = Integer.parseInt(br.readLine());
+		List<Data> list = new ArrayList<>();
+		for(int i=0;i<n;i++) {
+			String[] tmp = br.readLine().split(" ");
+			list.add(new Data(Integer.parseInt(tmp[0]),Integer.parseInt(tmp[1])));
+		}
+		
+		list.sort((o1,o2)->{
+			if(o1.d==o2.d) return Integer.compare(o2.w,o1.w);
+			return Integer.compare(o1.d, o2.d);
+		});
+		
+		PriorityQueue<Data> pq = new PriorityQueue<Data>(
+				(o1,o2)->{
+					if(o1.w==o2.w) return Integer.compare(o1.d, o2.d);
+					return Integer.compare(o1.w, o2.w);
+				}
+				);
+		int first = list.get(0).d;
+		for(int i=0, cnt=0;i<list.size();) { // cnt : 과제 개수
+			int day = list.get(i).d;
+			while(i<list.size() && list.get(i).d==day) {
+				if(pq.size()<day) {
+					pq.offer(list.get(i));
+				}else if(pq.size()==day) {
+					Data data = pq.peek();
+					if(data.w<list.get(i).w) {
+						pq.poll();
+						pq.offer(list.get(i));
+					}
+				}else {
+					pq.poll();
+				}
+				i++;
+			}
+//			System.out.println(pq);
+		}
+		
+		long answer = 0;
+		while(!pq.isEmpty()) answer+=pq.poll().w;
+		System.out.println(answer);
+		br.close();
+	}
+
+}
