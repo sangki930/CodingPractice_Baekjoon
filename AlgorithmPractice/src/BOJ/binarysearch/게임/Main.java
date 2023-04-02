305514f1bee71af50fd6d3e47b5755f46d4625de
sangki930 <sangki931@gmail.com>
2023-04-02 23:33:11 +0900

    commit

--- AlgorithmPractice/src/BOJ/binarysearch/게임/Main.java
+++ AlgorithmPractice/src/BOJ/binarysearch/게임/Main.java
@@ -1,4 +1,4 @@
-package BOJ.binarysearch.ê²Œìž„;
+package BOJ.binarysearch.°ÔÀÓ;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -6,33 +6,33 @@ import java.io.InputStreamReader;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		String input[]=br.readLine().split(" ");
-		
-		long x = Long.parseLong(input[0]);
-		long y = Long.parseLong(input[1]);
-		int z = getPercent(x,y);
-		long ans=-1;
-		long left=0;
-		long right = (long)1e9;
-		
-		while(left<=right) {
-			long mid = (left+right)/2;
-			if(getPercent(x+mid,y+mid)!=z) {
-				ans=mid;
-				right=mid-1;
-			}else
-				left = mid+1;
-		}
-		System.out.println(ans);
-		
-		br.close();
-	}
-	
-	static int getPercent(long x, long y) {
+    public static void main(String[] args) throws IOException {
+        // TODO Auto-generated method stub
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String input[]=br.readLine().split(" ");
+
+        long x = Long.parseLong(input[0]);
+        long y = Long.parseLong(input[1]);
+        int z = getPercent(x,y);
+        long ans=-1;
+        long left=0;
+        long right = (long)1e9;
+
+        while(left<=right) {
+            long mid = (left+right)/2;
+            if(getPercent(x+mid,y+mid)!=z) {
+                ans=mid;
+                right=mid-1;
+            }else
+                left = mid+1;
+        }
+        System.out.println(ans);
+
+        br.close();
+    }
+
+    static int getPercent(long x, long y) {
         return (int) (y * 100 / x);
     }
 
