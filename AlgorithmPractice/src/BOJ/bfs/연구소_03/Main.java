61d10c4ffd9be94ca60c09124db43ce2a493c037
sangki930 <sangki931@gmail.com>
2023-05-26 23:45:14 +0900

    commit 연구소 3

--- AlgorithmPractice/src/BOJ/bfs/연구소_03/Main.java
+++ AlgorithmPractice/src/BOJ/bfs/연구소_03/Main.java
@@ -1,8 +1,16 @@
 package BOJ.bfs.楷备家_03;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 // 楷备家 03
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+
+
+        br.close();
     }
 }
