46be76b965888601532fc18a98c0763946dfae55
sangki930 <sangki931@gmail.com>
2023-06-07 23:47:22 +0900

    commit 미친로봇

---
+++ AlgorithmPractice/src/BOJ/dfs/미친로봇/Main.java
@@ -0,0 +1,25 @@
+package BOJ.dfs.¹ÌÄ£·Îº¿;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        // TODO Auto-generated method stub
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] input = br.readLine().split(" ");
+
+        int n = Integer.parseInt(input[0]);
+        int east = Integer.parseInt(input[1]);
+        int west = Integer.parseInt(input[2]);
+        int south = Integer.parseInt(input[3]);
+        int north = Integer.parseInt(input[4]);
+
+
+        br.close();
+    }
+
+}
